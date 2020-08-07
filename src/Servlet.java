9379aea1a6344808049f5e5b6aeaa6e1f7117e34
SPOTO-TianMa <1684018881@qq.com>
2020-08-07 11:19:33 +0800

    first commit

--- src/Servlet.java
+++ src/Servlet.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Servlet extends javax.servlet.http.HttpServlet {
 
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        System.out.println("dopost");
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
