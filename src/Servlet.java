a50bb1b2be86c93d96b67318b09d034ed8fc682a
SPOTO-TianMa <1684018881@qq.com>
2020-08-07 11:25:42 +0800

    second commit

--- src/Servlet.java
+++ src/Servlet.java
@@ -5,6 +5,7 @@ public class Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println("dopost");
         System.out.println("dopost...");
+        System.out.println("dododo");
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
